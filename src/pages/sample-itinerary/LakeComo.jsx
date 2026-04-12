8a44b8c49e514e0dff6bc983d26ef5b1462263f1
Ryan <ryan@deriva.travel>
2026-04-12 16:19:01 -0400

    Switch Lake Como map style to streets-v12 for clear blue water

--- src/pages/sample-itinerary/LakeComo.jsx
+++ src/pages/sample-itinerary/LakeComo.jsx
@@ -108,7 +108,7 @@ function StaticLakeMap() {
 
       const map = new mapboxgl.Map({
         container: containerRef.current,
-        style: 'mapbox://styles/mapbox/light-v11',
+        style: 'mapbox://styles/mapbox/streets-v12',
         center: [9.18, 45.92],
         zoom: 10,
         interactive: true,
